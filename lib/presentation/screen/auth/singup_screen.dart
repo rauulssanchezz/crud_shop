69a2f3f8aaffcdf84b9c5cdbc98a3896285d88b3
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2025-07-08 19:14:34 +0200

    fix limit error

--- lib/presentation/screen/auth/singup_screen.dart
+++ lib/presentation/screen/auth/singup_screen.dart
@@ -50,7 +50,7 @@ class _FormState extends State<_Form> {
     return SafeArea(
       child: Center(
         child: Padding(
-          padding: const EdgeInsets.all(40.0),
+          padding: const EdgeInsets.fromLTRB(40.0, 40.0, 40.0, 0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
