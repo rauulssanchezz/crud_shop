7ee0a18f837f7edc74b97510facf5de9af8e13fc
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2025-07-08 18:26:44 +0200

    theme provisional

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static final ColorScheme _colorTheme = ColorScheme.fromSeed(seedColor: Colors.orange);
+  static final ColorScheme _colorTheme = ColorScheme(
+    brightness: Brightness.light,
+    primary: Color.fromRGBO(242, 130, 33, 1),
+    onPrimary: Color.fromRGBO(28, 20, 13, 1),
+    secondary: Color.fromRGBO(245, 237, 232, 1),
+    onSecondary: Color.fromRGBO(156, 112, 74, 1),
+    error: Colors.red.shade700,
+    onError: Colors.white,
+    surface: Color.fromRGBO(252, 250, 247, 1),
+    onSurface: Colors.black87,
+  );
 
   static ThemeData themeData() {
     return ThemeData(
