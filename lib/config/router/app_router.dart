2a6dd737ece5e773bac82e718449a32a9ecf86e6
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2025-07-08 18:26:54 +0200

    router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,14 +1,15 @@
-import 'package:crud_shop/presentation/screen/home/home.dart';
+import 'package:crud_shop/presentation/screen/auth/login.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
   static GoRouter router = GoRouter(
-    routes: <RouteBase> [
+    initialLocation: '/login',
+    routes: [
       GoRoute( 
-        path: '/',
+        path: '/login',
         builder: (BuildContext context, GoRouterState state) {
-          return const HomeScreen();
+          return const LoginScreen();
         }
       )
     ]
