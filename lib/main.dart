d0d1e7300d66f279c82c0770f7ea51c90f43265d
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2025-07-09 12:46:48 +0200

    add providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crud_shop/config/router/app_router.dart';
 import 'package:crud_shop/config/theme/app_theme.dart';
+import 'package:crud_shop/presentation/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthProvider(db: FirebaseFirestore.instance))
+      ],
+      child: const MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
