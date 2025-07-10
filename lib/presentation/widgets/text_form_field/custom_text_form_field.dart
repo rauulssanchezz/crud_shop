10ea0a24469a14ef399acdd2e3a0ac43a5dbd731
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2025-07-10 12:44:21 +0200

    create new textform

--- lib/presentation/widgets/text_form_field/custom_text_form_field.dart
+++ lib/presentation/widgets/text_form_field/custom_text_form_field.dart
@@ -43,3 +43,64 @@ class CustomTextFormField extends StatelessWidget {
     );
   }
 }
+
+class CustomTextFormFieldWithHint extends StatelessWidget {
+  const CustomTextFormFieldWithHint({
+    super.key,
+    required TextEditingController controller,
+    required FocusNode focusNode,
+    void Function(String)? onFieldSubmitted,
+    required String labelText,
+    required bool isPassword, 
+    required String topLabel
+  }) : 
+    _controller = controller, 
+    _focusNode = focusNode ,
+    _onFieldSubmitted = onFieldSubmitted,
+    _labelText = labelText,
+    _isPassword = isPassword,
+    _topLabel = topLabel;
+
+  final TextEditingController _controller;
+  final FocusNode _focusNode;
+  final void Function(String)? _onFieldSubmitted;
+  final String _labelText;
+  final bool _isPassword;
+  final String _topLabel;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 10.0),
+          child: Text(
+            _topLabel,
+            style: const TextStyle(
+              fontSize: 18,
+            ),
+          ),
+        ),
+        TextFormField(
+          controller: _controller,
+          focusNode: _focusNode,
+          decoration: InputDecoration(
+            labelText: _labelText,
+            labelStyle: TextStyle(color: Color.fromRGBO(156, 112, 74, 1)),
+            filled: true,
+            fillColor: Color.fromRGBO(245, 237, 232, 1),
+            border: OutlineInputBorder(borderSide: BorderSide.none, borderRadius: BorderRadius.circular(20))
+          ),
+          textInputAction: TextInputAction.next,
+          onFieldSubmitted: _onFieldSubmitted,
+          onTapOutside: (event) {
+            _focusNode.unfocus();
+          },
+          obscureText: _isPassword,
+        ),
+      ],
+    );
+  }
+}
