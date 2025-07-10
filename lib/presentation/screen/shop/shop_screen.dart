d194b639bd653e70c8834d90c0b74eba5bfbfc05
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2025-07-10 12:44:00 +0200

    rename screen

---
+++ lib/presentation/screen/shop/shop_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShopScreen extends StatelessWidget {
+  const ShopScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tienda'),
+        centerTitle: true, 
+      ),
+    );
+  }
+}
