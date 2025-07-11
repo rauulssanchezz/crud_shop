e80a1f2c2ddcff016e89fd8e3b7a7be7e3792b28
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2025-07-11 10:48:03 +0200

    init product provider

---
+++ lib/presentation/providers/product_provider.dart
@@ -0,0 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crud_shop/domain/models/product.dart';
+import 'package:flutter/material.dart';
+
+class ProductProvider extends ChangeNotifier {
+  final FirebaseFirestore _db;
+
+  ProductProvider({required FirebaseFirestore db}) : _db = db;
+
+  String _errorMessage = 'Error en el servidor';
+
+  Future<QuerySnapshot<Map<String, dynamic>>> _findProduct({required String name}) async {
+    QuerySnapshot<Map<String, dynamic>> querySnapshot;
+    
+    querySnapshot = await _db
+        .collection('products')
+        .where('name', isEqualTo: name)
+        .get();
+    
+
+    return querySnapshot;
+  }
+
+  Future<String> addProduct({
+    required String name,
+    required String description,
+    required double price,
+    required int stock,
+  }) async {
+    try {
+      final querySnapshot = await _findProduct(name: name);
+
+      if (querySnapshot.docs.isNotEmpty) {
+        _errorMessage = 'El usuario ya existe';
+        throw ErrorHint(_errorMessage);
+      }
+
+      final docRef = _db.collection('products').doc();
+      final String productId = docRef.id;
+
+      Map<String, dynamic> productData = {
+        'id': productId,
+        'name': name,
+        'description': description,
+        'price': price,
+        'stock': stock
+      };
+
+      await docRef.set(productData);
+    } catch(e) {
+      throw ErrorHint(_errorMessage);
+    }
+
+    return 'Producto guardado correctamente';
+  }
+}
